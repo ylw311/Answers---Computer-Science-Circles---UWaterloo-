4dc1eabbbe575442d32e14326929b881f7295793
Linda Wang <61102300+ylw24@users.noreply.github.com>
2020-07-03 12:29:14 -0400

    Update Unfinished: 15B Poetic Analysis.py

--- Unfinished: 15B Poetic Analysis.py
+++ Unfinished: 15B Poetic Analysis.py
@@ -1,1 +1,24 @@
 
+def countWords(S):
+   if S == e: 
+      return highestN
+   while S != e:
+      for i in range(len(list)):
+         for u in range(len(list)):
+   
+
+list=[]
+inp = input()
+lowp = inp.lower()
+splitp= lowp.split()
+list.append(splitp)
+e = '###'
+while inp != e:
+   inp = input()
+   lowp = inp.lower()
+   splitp= lowp.split()
+   list.append(splitp)
+
+highestN = 0
+
+countWords(splitp)

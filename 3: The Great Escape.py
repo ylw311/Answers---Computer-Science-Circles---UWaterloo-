205fa279f073c8941fe11a725ea649c69fa569f7
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-05 18:59:32 -0400

    Update 3: The Great Escape.py

--- 3: The Great Escape.py
+++ 3: The Great Escape.py
@@ -1,1 +1,4 @@
+#the three ways:
 print("A double-quote\'s escaped using a backslash, e.g. \\\"")
+print('A double-quote\'s escaped using a backslash, e.g. \\\"')
+print('A double-quote\'s escaped using a backslash, e.g. \\"')

425f12dfc7d7c63352254607f075b59ef99ca130
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-06 10:50:40 -0400

    Update 6: Age checker.py

--- 6: Age checker.py
+++ 6: Age checker.py
@@ -5,3 +5,13 @@ if age <= 17 and age >= 0:
    print("Too young to vote")
 if age < 0:
    print("You are a time traveller")
+
+ #--------------or
+
+age = int(input())
+if age >= 18:
+ print("You can vote")
+elif age < 0:
+ print("You are a time traveller")
+else:
+ print("Too young to vote")

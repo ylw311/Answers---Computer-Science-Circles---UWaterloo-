b60f07fedccb0aa1aad582c59fea64849c4b4f94
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-06 10:47:04 -0400

    Update 6: IHOPython.py

--- 6: IHOPython.py
+++ 6: IHOPython.py
@@ -4,3 +4,11 @@ if pancakes > 3 :
      print("Yum!")
 if pancakes <= 3 :
      print("Still hungry!")
+
+     
+#--------------or
+pancakes = int(input())
+if pancakes > 3:
+ print("Yum!")
+else:
+ print("Still hungry!")

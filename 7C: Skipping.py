43a22ef83599dfa2d8a9d5bfb1ae2377512b5526
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-06 19:34:53 -0400

    Update 7C: Skipping.py

--- 7C: Skipping.py
+++ 7C: Skipping.py
@@ -1,9 +1,8 @@
 counter = 0
 while True:
   lineIn = input()
-  if lineIn=="SKIP":
-     continue
-     counter = counter-1
+  if lineIn=='SKIP':
+      continue
   if lineIn=='END':
     break
   counter = counter+1

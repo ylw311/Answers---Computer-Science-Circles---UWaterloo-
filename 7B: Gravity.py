84aa3465d610e506b65d4ec5d1b2851282449e8c
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-06 14:09:53 -0400

    Update 7B: Gravity.py

--- 7B: Gravity.py
+++ 7B: Gravity.py
@@ -1,13 +1,13 @@
 import math
-
 sV = input()
-
 v = float(sV)
-
 t = v**2 - 4*-4.9*11000
-
 sq = math.sqrt(t)   
-
 t = (v - sq) / (2*-4.9)
-
 print(t)
+
+#----or
+
+import math
+v = float(input())
+print((v-math.sqrt(v**2-4*(-4.9)*(11000)))/(2*(-4.9)))

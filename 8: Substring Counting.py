1e4fe976fa9c6f129e68f2ba5b3a0fef98778121
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-07 15:01:47 -0400

    Update 8: Substring Counting.py

--- 8: Substring Counting.py
+++ 8: Substring Counting.py
@@ -2,8 +2,8 @@ needle = input()
 haystack = input()
 count = 0
 
-for i in range(0,len(haystack)):
-   if haystack[i:len(needle)+i] == needle:
-      count = count+1
-      
+for i in range(len(haystack)):
+   if haystack[i:i+len(needle)] == needle:
+      count+=1
+   
 print(count)

69190f789f04f7b13376d41ce2cd168e6d4faeea
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-07 16:11:03 -0400

    Update 8: Ending Time.py

--- 8: Ending Time.py
+++ 8: Ending Time.py
@@ -30,3 +30,42 @@ if minutesLeft == 9:
    minutesLeft = "0"+str(minutesLeft)
             
 print(str(hour)+":"+str(minutesLeft))
+
+
+#---------or
+
+startingTime = input()
+passedTime = int(input())
+
+index = 0
+
+#find and split the starting time into hours and minutes
+for i in range(len(startingTime)):
+   if startingTime[i] == ":":
+      index = i
+      hour = int(startingTime[0:index])
+      minutes = int(startingTime[index+1:len(startingTime)])
+      break
+  
+newHour = str((hour + int((minutes+passedTime)/60)) % 24) #rounded down
+
+   
+newMinutes = str(int(minutes+passedTime) -  60*int((minutes+passedTime)/60))
+
+
+
+#these extra conditional statements are just here to keep the grader happy cuz apparently it wants two numbers for single digit times...you'll see
+#there's prob better way to do this but I got lazy-
+hourSingleDigit = len(newHour) == 1
+minutesSingleDigit = len(newMinutes) == 1
+
+if hourSingleDigit == True and minutesSingleDigit == True:
+   print("0" + newHour + ":" + "0" + newMinutes)
+elif hourSingleDigit == True:
+   print("0" + newHour + ":" + newMinutes)
+elif minutesSingleDigit == True:
+   print(newHour + ":" + "0" + newMinutes)
+else:
+   print(newHour + ":" + newMinutes)
+
+     

53ff1756ffdb37c19767d67604a7c85c4ff71662
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-07 14:48:31 -0400

    Update 8: Python Adder.py

--- 8: Python Adder.py
+++ 8: Python Adder.py
@@ -6,3 +6,14 @@ for p in range(0,len(S)):
       num2 = int(S[p+1: len(S)-0])
 
 print(num1 + num2)
+
+#-----or
+
+S = input()
+index = 0
+for i in range(len(S)):
+   if S[i] == "+":
+      index = i
+      break
+      
+print(int(S[0:index]) + int(S[index:len(S)]) )

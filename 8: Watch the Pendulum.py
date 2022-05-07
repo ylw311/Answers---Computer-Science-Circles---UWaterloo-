53f4e537a5cecf3ae5781f09524b397bd055efad
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-07 15:11:36 -0400

    Update 8: Watch the Pendulum.py

--- 8: Watch the Pendulum.py
+++ 8: Watch the Pendulum.py
@@ -1,6 +1,6 @@
 import math
 L = float(input())
 A = float(input())
-for T in range(0,10):
-   X = L*math.cos(A*math.cos(T*math.sqrt(9.8/L))) - L*math.cos(A)
-   print(X)
+
+for T in range(10):
+   print(L * math.cos(A * math.cos(T * math.sqrt(9.8/L))) - L * math.cos(A))

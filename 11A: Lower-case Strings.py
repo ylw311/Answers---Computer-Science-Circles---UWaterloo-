cb27551ac510af21a6abc6ad39bf7bf03fc4b39c
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-07 23:21:09 -0400

    Update 11A: Lower-case Strings.py

--- 11A: Lower-case Strings.py
+++ 11A: Lower-case Strings.py
@@ -1,11 +1,10 @@
 def lowerChar(char):
-   if ord(char) >= ord('A') and ord(char) <= ord('Z'):
+   if char >= "A" and char <= "Z":
       return chr(ord(char) + 32)
-   else:
-      return (char)
+   return char
 
 def lowerString(string):
    result = ""
-   for i in range(0, len(string)):
+   for i in range(len(string)):
       result = result + lowerChar(string[i])
    return result

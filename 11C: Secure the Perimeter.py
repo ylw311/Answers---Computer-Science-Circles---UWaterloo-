99e2283c2d3622de8e32866d876634ec11e84cf9
Linda Wang <61102300+ylw24@users.noreply.github.com>
2022-05-08 14:10:45 -0400

    Update 11C: Secure the Perimeter.py

--- 11C: Secure the Perimeter.py
+++ 11C: Secure the Perimeter.py
@@ -1,3 +1,2 @@
 def trianglePerimeter(xA, yA, xB, yB, xC, yC):
-   p = distance2D(xA, yA, xB, yB) + distance2D(xB, yB, xC, yC) + distance2D(xC, yC, xA, yA)
-   return p
+   return distance2D(xA, yA, xB, yB) + distance2D(xB, yB, xC, yC) + distance2D(xA, yA, xC, yC)

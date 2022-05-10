fe83a49dff2243a5e37105474e816a32b0e295a7
Linda Wang <61102300+ylw311@users.noreply.github.com>
2022-05-10 15:24:27 -0400

    Update 13: Palindrome.py

--- 13: Palindrome.py
+++ 13: Palindrome.py
@@ -4,3 +4,25 @@ def isPalindrome(S):
          continue
       else: return False 
    return True
+
+#or a more unnecessary complicated version lol (which may help understand or be more intuitive to some?)
+
+import math
+
+def isPalindrome(S):
+   #if even number
+   if len(S)%2==0:
+       for i in range(math.ceil(len(S)/2)):
+            if S[i] == S[-(i+1)]:
+               continue
+            else:
+               return False
+       return True
+   #if odd number
+   elif len(S)%2!=0:
+       for i in range(int(len(S)/2)):
+            if S[i] == S[-(i+1)]:
+               continue
+            else:
+               return False
+       return True
